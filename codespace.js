68b72a6182d9f2a03db99381eb0fce968a96e8b4
Kajsa Andersson <kajsa2002@gmail.com>
2023-04-25 09:27:24 +0200

    få in theme customization

--- codespace.js
+++ codespace.js
@@ -1,4 +1,6 @@
 var taskComplete = false;
+let workspace = null;
+
 function openHint() {
   let x = document.getElementById("hint");
   if (x.style.display === "none") {
@@ -15,7 +17,7 @@ function checkmark() {
   }
 }
 
-const toolbox = {
+const categoryToolbox = {
   kind: "categoryToolbox",
   contents: [
     {
@@ -604,4 +606,67 @@ const toolbox = {
     },
   ],
 };
-const workspace = Blockly.inject("blocklyDiv", { toolbox: toolbox });
+
+Blockly.Themes.Halloween = Blockly.Theme.defineTheme("halloween", {
+  base: Blockly.Themes.Classic,
+  categoryStyles: {
+    list_category: {
+      colour: "#4a148c",
+    },
+    logic_category: {
+      colour: "#8b4513",
+    },
+    loop_category: {
+      colour: "#85E21F",
+    },
+    text_category: {
+      colour: "#FE9B13",
+    },
+  },
+  blockStyles: {
+    list_blocks: {
+      colourPrimary: "#4a148c",
+      colourSecondary: "#AD7BE9",
+      colourTertiary: "#CDB6E9",
+    },
+    logic_blocks: {
+      colourPrimary: "#8b4513",
+      colourSecondary: "#ff0000",
+      colourTertiary: "#C5EAFF",
+    },
+    loop_blocks: {
+      colourPrimary: "#85E21F",
+      colourSecondary: "#ff0000",
+      colourTertiary: "#C5EAFF",
+    },
+    text_blocks: {
+      colourPrimary: "#FE9B13",
+      colourSecondary: "#ff0000",
+      colourTertiary: "#C5EAFF",
+    },
+  },
+  componentStyles: {
+    workspaceBackgroundColour: "#dbeede",
+    toolboxBackgroundColour: "#4b6455",
+    toolboxForegroundColour: "#fff",
+    flyoutBackgroundColour: "#252526",
+    flyoutForegroundColour: "#ccc",
+    flyoutOpacity: 1,
+    scrollbarColour: "#ff0000",
+    insertionMarkerColour: "#fff",
+    insertionMarkerOpacity: 0.3,
+    scrollbarOpacity: 0.4,
+    cursorColour: "#d0d0d0",
+    blackBackground: "#333",
+  },
+});
+
+//const workspace = Blockly.inject("blocklyDiv", { toolbox: toolbox });
+
+function start() {
+  // Create main workspace.
+  workspace = Blockly.inject("blocklyDiv", {
+    toolbox: categoryToolbox,
+    theme: Blockly.Themes.Halloween,
+  });
+}
